7e52842a29648d6ec60b0a82349b9f4bc6bf7ba5
nbrabant <brabantnicolas59@gmail.com>
2020-11-07 16:17:59 +0100

    implement neightbour count

--- gameoflife.spec.js
+++ gameoflife.spec.js
@@ -1,15 +1,40 @@
 describe('Game of life', () => {
     it('Shoud have a cell with 8 neightbour', () => {
-        expect(getCell().countAliveNeightbour()).toBe(8);
+		let cell = getCell();
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+
+        expect(cell.countAliveNeightbour()).toBe(8);
 	});
 	
 	it('Should cell with 8 alive neightbours is dead', () => {
-		expect(getCell().isAlive()).toBeFalsy();
+		let cell = getCell();
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+
+		expect(cell.isAlive()).toBeFalsy();
 	});
 
 	it('Should cell with 3 alive neightbours is alive', () => {
-		expect(getCell().countAliveNeightbour()).toBe(3);
-		expect(getCell().isAlive()).toBeTruthy();
+		let cell = getCell();
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+		cell.addNeightbour(new Cell());
+			
+		expect(cell.countAliveNeightbour()).toBe(3);
+		expect(cell.isAlive()).toBeTruthy();
 	});
 });
 
@@ -18,11 +43,18 @@ function getCell() {
 }
 
 class Cell {
+
+	neightbour = [];
+
+	addNeightbour(neightbour) {
+		this.neightbour.push(neightbour)
+	}
+
 	isAlive() {
-		return false
+		return this.countAliveNeightbour() == 3 ? true : false
 	}
 
 	countAliveNeightbour() {
-		return 8;
+		return this.neightbour.length;
 	}
 }
